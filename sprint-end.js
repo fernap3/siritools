4bed5699a86e08a63219a0b9e1c15df9cc87e5f0
pfernandes <pfernandes@exagoinc.com>
2019-06-11 11:47:46 -0400

    Initial commit

---
+++ sprint-end.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const sql = require("@fernap3/sql");
+
+(async () => {
+	const rows = await sql.doQuery("SELECT * FROM Sprint", []);
+	
+	const currentSprint = rows.find(s => s.state === "active");
+	console.log(`The current sprint ends on ${new Intl.DateTimeFormat("en-US", { month: "long", day: "numeric" }).format(currentSprint.endDate)}`);
+
+	sql.closePool();
+})();
