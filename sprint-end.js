3c09d59f9e408e71b6e803267f0db9a21502b82c
pfernandes <pfernandes@exagoinc.com>
2019-06-11 12:17:04 -0400

    Only pull down the record we need

--- sprint-end.js
+++ sprint-end.js
@@ -5,9 +5,9 @@ dotenv.config();
 const sql = require("@fernap3/sql");
 
 (async () => {
-	const rows = await sql.doQuery("SELECT * FROM Sprint", []);
+	const rows = await sql.doQuery("SELECT * FROM Sprint WHERE state = 'active'", []);
 	
-	const currentSprint = rows.find(s => s.state === "active");
+	const currentSprint = rows[0];
 	console.log(`The current sprint ends on ${new Intl.DateTimeFormat("en-US", { month: "long", day: "numeric" }).format(currentSprint.endDate)}`);
 
 	sql.closePool();
